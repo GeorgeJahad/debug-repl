ae4b7e9be64daac3ba356ed33476003b3ef110cc
George Jahad <github@blackbirdsystems.net>
2010-01-31 23:20:35 -0800

    inced version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gjahad/debug-repl "0.1.0-SNAPSHOT"
+(defproject org.clojars.gjahad/debug-repl "0.2.0-SNAPSHOT"
   :description "Clojure REPL that is aware of surrounding lexical scope"
   :dependencies [[org.clojure/clojure "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
