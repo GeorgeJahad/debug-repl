e1e74b8328bae79d59ac07ddab051b618b8398d5
George Jahad <george@runa.com>
2010-12-19 18:05:40 -0800

    moved to 1.2 from 1.2 snapshot per jakemcc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.gjahad/debug-repl "0.3.0-SNAPSHOT"
+(defproject org.clojars.gjahad/debug-repl "0.3.1"
   :description "Clojure REPL that is aware of surrounding lexical scope"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
