da2ab174334e4dd11c2a525da2ab822b32f7618c
george jahad <george@georgejahad.com>
2013-08-18 22:43:44 -0700

    updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gjahad/debug-repl "0.3.1"
+(defproject org.clojars.gjahad/debug-repl "0.3.3"
   :description "Clojure REPL that is aware of surrounding lexical scope"
   :dependencies [[org.clojure/clojure "1.5.1"]])
 
